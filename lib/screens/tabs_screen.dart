0c6cf14548bd4569b3314013530df100011be79a
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2021-03-22 09:53:50 +0900

    tabscreenのやり方や細部を追加

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,39 +9,92 @@ class tabsScreen extends StatefulWidget {
 }
 
 class _tabsScreenState extends State<tabsScreen> {
+  // actionやwidgetなども入れることができるので、場合分けも可能となる
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      // 初期のタブ決め、0が一番目
-      // initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        // _selectedPageの()に自動でtabのindexが入るようになっている
+        // 理由は不明
+        onTap: _selectedPage,
+        // ただ下記のようになっていると考えられる
+        // つまり勝手に変数が存在すれば渡すということかも？
+        // onTap: (index) => _selectedPage(index),
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorite'),
+          ),
+        ],
       ),
     );
   }
 }
+
+// 上にタブがあるver
+// return DefaultTabController(
+//       length: 2,
+//       // 初期のタブ決め、0が一番目
+//       // initialIndex: 0,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Meals'),
+//           bottom: TabBar(
+//             tabs: <Widget>[
+//               Tab(
+//                 icon: Icon(
+//                   Icons.category,
+//                 ),
+//                 text: 'Categories',
+//               ),
+//               Tab(
+//                 icon: Icon(
+//                   Icons.star,
+//                 ),
+//                 text: 'Favorites',
+//               )
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: <Widget>[
+//             CategoriesScreen(),
+//             FavoritesScreen(),
+//           ],
+//         ),
+//       ),
+//     );
